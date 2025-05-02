8e8037a6bc9028bb8c6681ef49a4dbcd64f3be8e
youserstack <youserstack@gmail.com>
2025-05-02 19:15:50 +0900

    update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,5 @@
 const config = {
-  // plugins: ["@tailwindcss/postcss"],
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {
-      overrideBrowserslist: ["> 1%", "last 2 versions", "iOS >= 10", "Safari >= 10"],
-    },
-  },
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
